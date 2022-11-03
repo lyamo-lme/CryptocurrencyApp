bd1a2290aac1d8fd4136a81f6a10cef3ea5d8da9
Maks Limont <91567557+ki212-lme@users.noreply.github.com>
2022-11-03 23:19:09 +0200

    changed Crypto class and had tested CoinCap API

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,17 +16,40 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TestTask.Model;
 
 namespace TestTask
 {
+    //https://docs.coincap.io/
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Cryptocurrency> list = new List<Cryptocurrency>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        async void Fetch()
+        {
+            
+                using (var client = new HttpClient())
+                {
+                    var response = await client.GetAsync("https://api.coincap.io/v2/assets");
+                    var ret = JsonConvert.DeserializeObject<dynamic>(await response.Content.ReadAsStringAsync());
+                    if (ret != null)
+                    {
+                        list = JsonConvert.DeserializeObject<List<Cryptocurrency>>(ret["data"].ToString());
+                    }
+                }
+            
+        }
+
+        private void Click(object sender, RoutedEventArgs e)
+        {
+            Fetch();
         }
     }
 }
--- Model/Cryptocurrency.cs
+++ Model/Cryptocurrency.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace TestTask.Model
 {
-    internal class Cryptocurrency
+    public class Cryptocurrency
     {
+        public string Id { get; set; }
+        public int Rank { get; set; }
+        public string Symbol { get;set; }
+        public string Name { get;set; }
+        public decimal Supply { get;set; }
+        public decimal? MaxSupply { get;set; }
+        public decimal MarketCapUsd { get;set; }
+        public decimal PriceUsd { get;set; }
+        public decimal ChangePercent24Hr { get;set; }
+        public decimal? VWap24Hr { get;set; }
+        
     }
 }
